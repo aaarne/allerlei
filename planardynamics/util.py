03ff98b388ef0d36847686d0abd73a9cf0b56d08
Arne Sachtler <arne.sachtler@dlr.de>
2022-01-21 15:10:19 +0100

    Make plotting function plot every pendulum

--- planardynamics/util.py
+++ planardynamics/util.py
@@ -6,27 +6,33 @@ _animations = list()
 
 def plot_pendulum_trajectory(pendulum, t, traj,
                              plot_orientation=True,
-                             energy=True,
+                             energy=False,
                              phase_plots=True,
                              plot_fkin=False,
                              auto_corr=False,
                              streamer=None,
+                             q0=None,
                              **kwargs):
+    n = pendulum.dof
+    q = traj[:, 0::2]
+    dq = traj[:, 1::2]
+    qlabels = {i: f'$q_{i + 1}$' for i in range(n)}
+    dqlabels = {i: f'$\\omega_{i+1}$' for i in range(n)}
     labels = {
-        0: '$q_1$',
-        2: '$q_2$',
-        1: '$\\omega_1$',
-        3: '$\\omega_2$',
+        **{2 * i: f'$q_{i + 1}$' for i in range(n)},
+        **{2*i+1: f'$\\omega_{i+1}$' for i in range(n)}
     }
+
     figures = []
-    delta = traj[:, 0::2] - pendulum.equilibrium
     f, axes = plt.subplots(3, 1)
-    axes[0].plot(t, traj[:, 0], label=labels[0])
-    axes[0].plot(t, traj[:, 2], label=labels[2])
-    axes[1].plot(t, delta[:, 0], label='$\\Delta q_1$')
-    axes[1].plot(t, delta[:, 1], label='$\\Delta q_2$')
-    axes[2].plot(t, traj[:, 1], label=labels[1])
-    axes[2].plot(t, traj[:, 3], label=labels[3])
+    for i in range(n):
+        axes[0].plot(t, q[:, i], label=qlabels[i])
+    if q0:
+        delta = q - pendulum.equilibrium
+        for i in range(n):
+            axes[1].plot(t, delta[:, i], label=f'$\\Delta q_{i+1}$')
+    for i in range(n):
+        axes[2].plot(t, dq[:, i], label=dqlabels[i])
     for ax in axes:
         ax.grid()
         ax.legend()
@@ -36,27 +42,33 @@ def plot_pendulum_trajectory(pendulum, t, traj,
     if auto_corr:
         f, ax = plt.subplots()
         from scipy.signal import correlate
-        ax.plot(correlate(delta[:, 0], delta[:, 0]))
-        ax.plot(correlate(delta[:, 1], delta[:, 1]))
+        if q0:
+            delta = q - pendulum.equilibrium
+            for i in range(n):
+                ax.plot(correlate(delta[:, i], delta[:, i]))
+        else:
+            for i in range(n):
+                ax.plot(correlate(q[:, i], q[:, i]))
 
-    if phase_plots:
-        f, axes = plt.subplots(2, 2)
-        for ax, a, b in [
-            (axes[0, 0], 0, 2),
-            (axes[0, 1], 1, 3),
-            (axes[1, 0], 0, 1),
-            (axes[1, 1], 2, 3),
-        ]:
-            ax.plot(traj[:, a], traj[:, b])
-            ax.set_xlabel(labels[a])
-            ax.set_ylabel(labels[b])
-            ax.grid()
-        f.tight_layout()
-        figures.append(f)
+    if n == 2:
+        if phase_plots:
+            f, axes = plt.subplots(2, 2)
+            for ax, a, b in [
+                (axes[0, 0], 0, 2),
+                (axes[0, 1], 1, 3),
+                (axes[1, 0], 0, 1),
+                (axes[1, 1], 2, 3),
+            ]:
+                ax.plot(traj[:, a], traj[:, b])
+                ax.set_xlabel(labels[a])
+                ax.set_ylabel(labels[b])
+                ax.grid()
+            f.tight_layout()
+            figures.append(f)
 
     if plot_fkin:
         f, axt = plt.subplots()
-        fkin = pendulum.forward_kinematics(traj[:, 0::2])
+        fkin = pendulum.forward_kinematics(q)
         handles = [
             axt.plot(t, fkin[:, 0], label='x'),
             axt.plot(t, fkin[:, 1], label='y'),
@@ -69,7 +81,8 @@ def plot_pendulum_trajectory(pendulum, t, traj,
         f.tight_layout()
         figures.append(f)
 
-    if energy:
+    kinfun = getattr(pendulum, 'kinetic_energy', None)
+    if energy and callable(kinfun):
         f, ax = plt.subplots()
         K = pendulum.kinetic_energy(traj[:, 0::2], traj[:, 1::2])
         V = pendulum.potential_energy(traj[:, 0::2])
@@ -84,10 +97,10 @@ def plot_pendulum_trajectory(pendulum, t, traj,
 
     f, ax = plt.subplots()
     link_fkin = pendulum.forward_kinematics_for_each_link(traj[:, 0::2])
-    ax.plot(link_fkin[:, 1, 0], link_fkin[:, 1, 1], alpha=.3)
-    ax.plot(link_fkin[:, 0, 0], link_fkin[:, 0, 1], alpha=.3)
+    for i in range(pendulum.dof):
+        ax.plot(link_fkin[:, i, 0], link_fkin[:, i, 1], alpha=.3)
     plot = RobotPlot(ax, f=f)
-    anim = plot.animated_trajectory(pendulum, traj[:, 0::2], t=t, streamer=streamer)
+    anim = plot.animated_trajectory(pendulum, q, t=t, streamer=streamer)
     _animations.append(anim)
     figures.append(f)
 
