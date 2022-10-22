fefc92496840a130fc5fef173ae25fcaec30b560
Arne Sachtler <arne.sachtler@gmail.com>
2022-10-22 16:45:29 +0200

    Pippify

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="allerlei",
+    version="0.0.1",
+    author="Arne Sachtler",
+    author_email="arne.sachtler@tum.de",
+    description="My Toolbox for Everything in Python",
+    url="https://github.com/aaarne/allerlei",
+    packages=['allerlei'],
+)
